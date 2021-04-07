e28082f13985df9c32379f9ceafe90ef2e54e7c2
Juan Henriquez <juanjoaquino05@gmail.com>
2021-04-06 21:54:42 -0400

    Initial Structure and Get All Shopping Lists

--- main.go
+++ main.go
@@ -3,24 +3,31 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	// "io/ioutil"
 	"log"
 	"net/http"
-	"strconv"
+	// "strconv"
 
 	"github.com/go-chi/chi"
 )
+type Item struct {
+	name string
+	quantity string
+	status string
+}
 
-var messages map[int]string
-var count int
+type ShoppingList struct {
+	title string
+	description string
+	fecha string
+	items []Item
+}
+
+var shoppingLists map[int]ShoppingList
 
 func main() {
 	port := "5000"
-	messages = map[int]string{
-		1: "Hola",
-		2: "Mundo",
-	}
-	count = 3
+	shoppingLists = map[int]ShoppingList{}
 	r := registerRoutes()
 	fmt.Println("Listening on port :" + port)
 	log.Fatal(http.ListenAndServe(":"+port, r))
@@ -30,78 +37,78 @@ func registerRoutes() http.Handler {
 	r := chi.NewRouter()
 
 	r.Route("/", func(r chi.Router) {
-		r.Get("/messages", getMessages)
-		r.Post("/messages", createMessage)
-		r.Put("/messages/{msgId}", updateMessage)
-		r.Delete("/messages/{msgId}", deleteMessage)
+		r.Get("/shoppinglist", getShoppingLists)
+		// r.Get("/shoppinglist/{msgId}", getMessages)
+		// r.Post("/shoppinglist", createShoppingList)
+		// r.Put("/shoppinglist/{msgId}", updateMessage)
+		// r.Delete("/shoppinglist/{msgId}", deleteMessage)
 	})
 	return r
 }
 
-func getMessages(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(messages)
+func getShoppingLists(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(shoppingLists)
 }
 
-func createMessage(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Invalid message", 400)
-		return
-	}
-	if len(body) == 0 {
-		http.Error(w, "Empty request body", 400)
-		return
-	}
+// func createShoppingList(w http.ResponseWriter, r *http.Request) {
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	if err != nil {
+// 		http.Error(w, "Invalid message", 400)
+// 		return
+// 	}
+// 	if len(body) == 0 {
+// 		http.Error(w, "Empty request body", 400)
+// 		return
+// 	}
 
-	messages[count] = string(body)
-	count += 1
-}
+// 	shoppingLists[] = string(body)
+// }
 
-func updateMessage(w http.ResponseWriter, r *http.Request) {
-	msgId := chi.URLParam(r, "msgId")
-	body, err := ioutil.ReadAll(r.Body)
-	if msgId == "" {
-		http.Error(w, "Empty message id", 400)
-		return
-	}
-	if err != nil {
-		http.Error(w, "Invalid message", 400)
-		return
-	}
-	if len(body) == 0 {
-		http.Error(w, "Empty request body", 400)
-		return
-	}
-	id, err := strconv.Atoi(msgId)
-	if err != nil {
-		http.Error(w, "Invalid message id", 400)
-		return
-	}
+// func updateMessage(w http.ResponseWriter, r *http.Request) {
+// 	msgId := chi.URLParam(r, "msgId")
+// 	body, err := ioutil.ReadAll(r.Body)
+// 	if msgId == "" {
+// 		http.Error(w, "Empty message id", 400)
+// 		return
+// 	}
+// 	if err != nil {
+// 		http.Error(w, "Invalid message", 400)
+// 		return
+// 	}
+// 	if len(body) == 0 {
+// 		http.Error(w, "Empty request body", 400)
+// 		return
+// 	}
+// 	id, err := strconv.Atoi(msgId)
+// 	if err != nil {
+// 		http.Error(w, "Invalid message id", 400)
+// 		return
+// 	}
 
-	if _, exists := messages[id]; exists {
-		messages[id] = string(body)
-	} else {
-		http.Error(w, "Message id does not exist", 400)
-		return
-	}
-}
+// 	if _, exists := messages[id]; exists {
+// 		messages[id] = string(body)
+// 	} else {
+// 		http.Error(w, "Message id does not exist", 400)
+// 		return
+// 	}
+// }
 
-func deleteMessage(w http.ResponseWriter, r *http.Request) {
-	msgId := chi.URLParam(r, "msgId")
-	if msgId == "" {
-		http.Error(w, "Empty message id", 400)
-		return
-	}
-	id, err := strconv.Atoi(msgId)
-	if err != nil {
-		http.Error(w, "Invalid message id", 400)
-		return
-	}
+// func deleteMessage(w http.ResponseWriter, r *http.Request) {
+// 	msgId := chi.URLParam(r, "msgId")
+// 	if msgId == "" {
+// 		http.Error(w, "Empty message id", 400)
+// 		return
+// 	}
+// 	id, err := strconv.Atoi(msgId)
+// 	if err != nil {
+// 		http.Error(w, "Invalid message id", 400)
+// 		return
+// 	}
 
-	if _, exists := messages[id]; exists {
-		delete(messages, id)
-	} else {
-		http.Error(w, "Message id does not exist", 400)
-		return
-	}
-}
+// 	if _, exists := messages[id]; exists {
+// 		delete(messages, id)
+// 	} else {
+// 		http.Error(w, "Message id does not exist", 400)
+// 		return
+// 	}
+// }
