28755fbc54ae9d2c0c9069a6a42f999824237708
Jose <jose.berges23@gmail.com>
2021-04-07 22:52:07 -0400

    Add PUT /shoppinglist/{id}

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"log"
 	"net/http"
@@ -18,9 +19,8 @@ type Item struct {
 }
 
 type ShoppingList struct {
-	Title       string
 	Description string
-	Fecha       string
+	Date        string
 	Items       map[string]Item //key: item name
 }
 
@@ -43,7 +43,7 @@ func registerRoutes() http.Handler {
 		r.Get("/shoppinglist", getShoppingLists)
 		// r.Get("/shoppinglist/{msgId}", getMessages)
 		r.Post("/shoppinglist", createShoppingList)
-		// r.Put("/shoppinglist/{msgId}", updateMessage)
+		r.Put("/shoppinglist/{id}", updateShoppingList)
 		// r.Delete("/shoppinglist/{msgId}", deleteMessage)
 	})
 	return r
@@ -74,34 +74,38 @@ func createShoppingList(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// func updateMessage(w http.ResponseWriter, r *http.Request) {
-// 	msgId := chi.URLParam(r, "msgId")
-// 	body, err := ioutil.ReadAll(r.Body)
-// 	if msgId == "" {
-// 		http.Error(w, "Empty message id", 400)
-// 		return
-// 	}
-// 	if err != nil {
-// 		http.Error(w, "Invalid message", 400)
-// 		return
-// 	}
-// 	if len(body) == 0 {
-// 		http.Error(w, "Empty request body", 400)
-// 		return
-// 	}
-// 	id, err := strconv.Atoi(msgId)
-// 	if err != nil {
-// 		http.Error(w, "Invalid message id", 400)
-// 		return
-// 	}
+func updateShoppingList(w http.ResponseWriter, r *http.Request) {
+	msgId := chi.URLParam(r, "id")
+	if msgId == "" {
+		http.Error(w, "Empty message id", 400)
+		return
+	}
 
-// 	if _, exists := messages[id]; exists {
-// 		messages[id] = string(body)
-// 	} else {
-// 		http.Error(w, "Message id does not exist", 400)
-// 		return
-// 	}
-// }
+	if r.Body == nil {
+		http.Error(w, "Empty request body", 400)
+		return
+	}
+	id, err := strconv.Atoi(msgId)
+	if err != nil {
+		http.Error(w, "Invalid message id", 400)
+		return
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	var newShoppingList ShoppingList
+	err = decoder.Decode(&newShoppingList)
+	if err != nil {
+		http.Error(w, "Could not parse request body", 400)
+		return
+	}
+
+	if _, exists := shoppingLists[id]; exists {
+		shoppingLists[id] = newShoppingList
+	} else {
+		http.Error(w, "Shopping list id does not exist", 404)
+		return
+	}
+}
 
 // func deleteMessage(w http.ResponseWriter, r *http.Request) {
 // 	msgId := chi.URLParam(r, "msgId")
